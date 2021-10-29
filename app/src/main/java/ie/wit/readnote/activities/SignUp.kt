f5d2bf751cf77be96cb835612882d6a25675f61e
Shane Ryan <shane.ryan@errigal.com>
2021-10-29 14:47:31 +0100

    Add check for existing username

--- app/src/main/java/ie/wit/readnote/activities/SignUp.kt
+++ app/src/main/java/ie/wit/readnote/activities/SignUp.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import ie.wit.readnote.R
 import ie.wit.readnote.databinding.ActivitySignUpBinding
 import ie.wit.readnote.main.readNoteApp
@@ -24,16 +25,29 @@ class SignUp : AppCompatActivity() {
         signUpLayout.SignUp.setOnClickListener {
             Timber.i("SIGN UP")
             var user = UserModel()
+            var userFound = false
+            val userNameEntry = signUpLayout.userName.text.toString()
             user.userName = signUpLayout.userName.text.toString()
             user.password = signUpLayout.password.text.toString()
-            app.data.createUser(user)
-            app.loggedInUser = user
-            i("USER CREATED $user")
-            app.setUser(user)
-            i("LOGGED IN USER ${app.loggedInUser}")
-            app.setUser(user)
-            setResult(RESULT_OK)
-            startActivity(Intent(this, BookList::class.java))
+            val users = app.data.getAllUsers()
+            users.forEach { if(user.userName == userNameEntry) {
+                userFound = true
+            } }
+            if(!userFound) {
+                app.data.createUser(user)
+                app.loggedInUser = user
+                i("USER CREATED $user")
+                app.setUser(user)
+                i("LOGGED IN USER ${app.loggedInUser}")
+                app.setUser(user)
+                setResult(RESULT_OK)
+                startActivity(Intent(this, BookList::class.java))
+            }
+            else {
+                Snackbar
+                    .make(it, R.string.snackbar_UsernameTaken, Snackbar.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 

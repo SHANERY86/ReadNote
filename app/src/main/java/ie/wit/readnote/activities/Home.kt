ba58eed0351d0f333fdb72fc48a38b02ec8f158f
Shane Ryan <shane.ryan@errigal.com>
2021-12-14 22:17:10 +0000

    nav drawer accessible

--- app/src/main/java/ie/wit/readnote/activities/Home.kt
+++ app/src/main/java/ie/wit/readnote/activities/Home.kt
@@ -2,11 +2,28 @@ package ie.wit.readnote.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ie.wit.readnote.R
+import ie.wit.readnote.databinding.HomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
